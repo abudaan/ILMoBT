6600cff10cf0fc99f949ec0c47aa3629004ffbf1
abudaan <daniel@abumarkub.net>
2020-05-20 21:39:01 +0200

    fixed small issue in download file

--- editor/src/components/Debug.tsx
+++ editor/src/components/Debug.tsx
@@ -7,13 +7,18 @@ export const Debug = (): JSX.Element => {
   const playheadPixels = useSelector((state: RootState) => state.playheadPixels);
   const transport = useSelector((state: RootState) => state.transport);
   const editAction = useSelector((state: RootState) => state.editAction);
+  const notes = useSelector((state: RootState) => state.notes);
+  const list = notes.map(n => {
+    return <li>{`${n.id} ${n.ticks} ${n.duration}`}</li>;
+  });
 
   return (
     <div className="debug">
-      <ul>
+      {/* <ul>
         <li>{transport}</li>
         <li>{editAction}</li>
-      </ul>
+      </ul> */}
+      <ul>{list}</ul>
     </div>
   );
 };
--- editor/src/redux/actions/saveMIDIFile.ts
+++ editor/src/redux/actions/saveMIDIFile.ts
@@ -2,22 +2,20 @@ import { store } from "../store";
 import { RootState } from "../../types";
 import { SAVE_MIDI_FILE } from "../../constants";
 import { unschedule } from "../../../../webdaw/unschedule";
-import { getNoteName } from "../../../../webdaw/getNoteName";
 import { midiAccess } from "../../media";
 import { clock, getMIDIFile } from "../../util/midi_utils";
 
 export const saveMIDIFile = () => {
   clock.stop();
   const state = store.getState() as RootState;
-  console.log(state.form);
   const { songData, notes } = state;
   unschedule(songData.song, midiAccess.outputs);
 
   const { noteMapping } = songData;
   const url = getMIDIFile(songData, notes, noteMapping);
-  const a: HTMLLinkElement = document.createElement("A") as HTMLLinkElement;
+  const a: HTMLAnchorElement = document.createElement("A") as HTMLAnchorElement;
   a.href = url;
-  a["download"] = "touch";
+  a.download = "touch";
   a.click();
   window.URL.revokeObjectURL(url); // cleanup
 
